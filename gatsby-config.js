5bbb75879df89039b18e8202dad52bf8837a4af4
Vladyslav Tkachenko <vlad@wtf.lv>
2023-02-24 15:14:18 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `turn-gatsby-website`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-sitemap"]
+};
