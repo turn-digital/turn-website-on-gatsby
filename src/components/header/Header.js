4694e438c5646811b7d46ca429b8d1892264aedb
Artem Vovk <artem@turn.lv>
2023-03-17 17:20:52 +0200

    add header and delete not needed png

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,24 +13,28 @@ const Header = () => {
         <header className="header">
           <Link className="header__logo"  to="/"><img className="header__img" src={Logo} alt="logo" /></Link>
           <div className="header__menu">
-            <div className="header__menu--mob">
+            <div>
             <ReactModal
+              className="header__menu--mob wrapper"
              isOpen={toggleMenu}
              contentLabel={"Navigation modal"}
              shouldFocusAfterRender={true}
              shouldCloseOnEsc={true}
              preventScroll={true}>
+             <div className="header__box">
+             <Link className="header__logo" to="/" onClick={() => setToggleMenu(!toggleMenu)}><img className="header__img" src={Logo} alt="logo" /></Link>
+               <button
+                  className="header__button header__button--close"
+                  onClick={() => setToggleMenu(!toggleMenu)}
+                >
+                  <StaticImage  src="../../assets/images/svg/menu-close.svg" alt="menu close" />
+                </button>
+             </div>
               <nav className="header__nav header__nav--mob">
               <Link className="header__link" to="/" onClick={() => setToggleMenu(!toggleMenu)}>Home</Link>
               <Link className="header__link" to="/#story" onClick={() => setToggleMenu(!toggleMenu)}>Story</Link>
               <Link className="header__link" to="/#services" onClick={() => setToggleMenu(!toggleMenu)}>Services</Link>
               <Link className="header__link" to="/#contacts" onClick={() => setToggleMenu(!toggleMenu)}>Contacts</Link>
-                <button
-                  className="header__button header__button--close"
-                  onClick={() => setToggleMenu(!toggleMenu)}
-                >
-                  X
-                </button>
               </nav>
             </ReactModal>
             </div>
